7f9a7078db7d70a40dad6c4b7b3f69347e9dc682
Ritika <tomarviii88@gmail.com>
2019-06-26 22:56:23 +0530

    Updates

---
+++ precache-manifest.00b6618e3ca81c3a72367c984eae10b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babda392248954aa958c5fb5122eb270",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "decc247c6f2fa7886b8a",
+    "url": "/memegenerator/static/css/main.180f97a7.chunk.css"
+  },
+  {
+    "revision": "ee468e830b966797fb7e",
+    "url": "/memegenerator/static/js/2.a6b2bd47.chunk.js"
+  },
+  {
+    "revision": "decc247c6f2fa7886b8a",
+    "url": "/memegenerator/static/js/main.a4658cdb.chunk.js"
+  },
+  {
+    "revision": "e40966f54bc993d0e999",
+    "url": "/memegenerator/static/js/runtime~main.71a76f7c.js"
+  }
+]);
